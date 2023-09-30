06fe9402cefbd91daf0554d4b51ff1de4ab7c44e
Ahmed-M-Anis <ahmed.m.anis65@gmail.com>
2023-09-30 18:41:37 +0300

    handel securety (query injection ,Sanitize xss ,limit requist , http secury

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+
 const productRouter = require("./router/productRouter.js");
 const userRouter = require("./router/userRouter.js");
 const sendingEorror = require("./controller/errorController.js");
+const config = require("./config.js");
 
 const app = express();
 
+//http secury
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: config.requistLimit,
+  message: "too many request please try again after an hour",
+});
+
+// limit the number of requist for one ip
+app.use(limiter);
+
 app.use(express.json());
+
+//data Sanitize protiction form data query injection
+app.use(mongoSanitize());
+
+//data Sanitize protiction form xss
+app.use(xss());
 
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/user", userRouter);
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
   tokenExpriresIn: 30 * 24 * 60 * 60 * 1000, //30 day
   resetTokenExpires: 10 * 60 * 1000, //10 min
+  requistLimit: 60 * 60 * 1000, // houre
 };
 
 module.exports = config;
