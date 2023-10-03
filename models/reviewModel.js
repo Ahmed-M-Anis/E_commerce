97a587ff615fa24e4621bcc037e97545e489d210
Ahmed-M-Anis <ahmed.m.anis65@gmail.com>
2023-10-03 18:45:11 +0300

    update average rate after every rate

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Product = require("./productModel");
 
 const reviewSchema = mongoose.Schema(
   {
@@ -38,6 +39,38 @@ reviewSchema.index({ product: 1, user: 1 }, { unique: true });
 reviewSchema.pre(/^find/, function (next) {
   this.populate({ path: "user", select: "name" });
   next();
+});
+
+reviewSchema.statics.getAvrageRating = async function (productId) {
+  const stats = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: "$product",
+        ratingNumber: { $sum: 1 },
+        avregeRate: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  if (!stats[0]) {
+    await Product.findByIdAndUpdate(productId, {
+      ratingNumber: 0,
+      avregeRate: 4.5,
+    });
+  }
+  await Product.findByIdAndUpdate(productId, {
+    ratingNumber: stats[0].ratingNumber,
+    avregeRate: stats[0].avregeRate,
+  });
+};
+
+reviewSchema.post("save", function () {
+  this.constructor.getAvrageRating(this.product);
+});
+
+reviewSchema.post(/^findOneAnd/, async function (doc) {
+  await doc.constructor.getAvrageRating(doc.product);
 });
 
 const Review = mongoose.model("review", reviewSchema);
