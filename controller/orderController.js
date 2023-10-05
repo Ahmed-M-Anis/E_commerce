16315f2a38ef05579b78c8e22456483e094942e2
Ahmed-M-Anis <ahmed.m.anis65@gmail.com>
2023-10-05 14:59:17 +0300

    clean the code

--- controller/orderController.js
+++ controller/orderController.js
@@ -23,14 +23,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
         price: price.id,
         quantity: 1,
       },
     ],
     mode: "payment",
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    success_url: `${YOUR_DOMAIN}/public/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/public/cancel.html`,
   });
 
   res.status(200).json({
