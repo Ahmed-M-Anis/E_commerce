5dfad95f07fbbf7b081bd08c9b154d4f39c846f9
Ahmed-M-Anis <ahmed.m.anis65@gmail.com>
2023-10-07 15:10:23 +0300

    recode the product model

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,42 +1,34 @@
 const mongoose = require("mongoose");
 
 const cartSchema = mongoose.Schema({
-  product: {
-    type: mongoose.Schema.ObjectId,
-    ref: "product",
-  },
+  product: [
+    {
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: "product",
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    },
+  ],
   user: {
     type: mongoose.Schema.ObjectId,
     ref: "user",
     required: [true, "cart must have user"],
     unique: true,
   },
-  totalPricr: Number,
+  totalPrice: Number,
 });
 
-cartSchema.statics.getTotal = async function (userId) {
-  const stats = await this.aggregate([
-    { $match: { user: userId } },
-    {
-      $group: {
-        _id: "$user",
-        totalPricr: { $sum: "$product.price" },
-      },
-    },
-  ]);
-  console.log(stats);
-};
-
-cartSchema.pre(/^find/, function (next) {
-  this.populate({
-    path: "product",
-    select: "name image price",
-  });
-  next();
-});
-
-cartSchema.post(/^find/, async function (doc) {
-  await doc.constructor.getAvrageRating(doc.user);
+cartSchema.virtual("totalPrice").get(function () {
+  let totalPrice = 0;
+  // Iterate through the products and calculate the total price
+  for (const item of this.product) {
+    totalPrice += item.product.price * item.quantity;
+  }
+  return totalPrice;
 });
 
 const Cart = mongoose.model("cart", cartSchema);
