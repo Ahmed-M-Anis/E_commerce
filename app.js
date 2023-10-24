ef6db897ac2c2d44b601e65922dffbd1f7b0b2d2
Ahmed-M-Anis <ahmed.m.anis65@gmail.com>
2023-10-24 20:35:04 +0300

    a

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const config = require("./config.js");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "'http://localhost'");
   next();
 });
 //http secury
